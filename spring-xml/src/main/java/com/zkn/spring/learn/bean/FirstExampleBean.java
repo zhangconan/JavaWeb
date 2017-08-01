c4ef8df9646982f2fed435e26db26bd15b8ba82f
ConanZhang <zhangconancomputer@126.com>
2017-08-01 08:19:37 +0800

    新增读取配置文件

---
+++ spring-xml/src/main/java/com/zkn/spring/learn/bean/FirstExampleBean.java
@@ -0,0 +1,19 @@
+package com.zkn.spring.learn.bean;
+
+/**
+ * Created by zkn on 2017/8/1.
+ */
+public class FirstExampleBean  {
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}

b173fc088d012ea024dd8d3548ce84d760d14cba
ConanZhang <zhangconancomputer@126.com>
2017-08-30 23:26:51 +0800

    新增Spring事件发布器单测

--- spring-annotation/src/main/java/com/zkn/spring/listener/MyEventReceiver.java
+++ spring-annotation/src/main/java/com/zkn/spring/listener/MyEventReceiver.java
@@ -11,11 +11,17 @@ public class MyEventReceiver {
 
     /**
      * 监听事件方法
+     * 只监听MyEventDemo1事件
      *
      * @param event
      */
-    @EventListener
+    @EventListener(value = MyEventDemo1.class)
     public void handlerEvent(Object event) {
         System.out.println("我是事件监听器：" + event);
     }
+
+//    @EventListener(value = MyEventDemo1.class)
+//    public void handlerEvent(Object event) {
+//        System.out.println("我是事件监听器：" + event);
+//    }
 }
--- spring-annotation/src/test/java/com/zkn/spring/listener/MyEventPublisherTest.java
+++ spring-annotation/src/test/java/com/zkn/spring/listener/MyEventPublisherTest.java
@@ -1,6 +1,7 @@
 package com.zkn.spring.listener;
 
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by zkn on 2017/8/30.
@@ -8,8 +9,10 @@ import org.junit.Test;
 public class MyEventPublisherTest {
 
     @Test
-    public void test(){
+    public void test() {
 
-
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/zkn/spring/listener/applicationContext.xml");
+        MyEventSender myEventSender = context.getBean(MyEventSender.class);
+        myEventSender.publishEvent();
     }
 }

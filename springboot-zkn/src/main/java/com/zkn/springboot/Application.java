0759d16f99d581b6a820bbe5318c4c00ba86546a
ConanZhang <zhangconancomputer@126.com>
2017-09-03 09:51:22 +0800

    新增SpringBoot

---
+++ springboot-zkn/src/main/java/com/zkn/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.zkn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zkn on 2017/8/31.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}

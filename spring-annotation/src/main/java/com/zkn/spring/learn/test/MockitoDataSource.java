2432016d10b5910d626b3374f648f55bce2aaf65
ConanZhang <zhangconancomputer@126.com>
2017-09-11 23:22:26 +0800

    新增Mockito单测

--- spring-annotation/src/main/java/com/zkn/spring/learn/test/MockitoDataSource.java
+++ spring-annotation/src/main/java/com/zkn/spring/learn/test/MockitoDataSource.java
@@ -1,6 +1,8 @@
 package com.zkn.spring.learn.test;
 
 import com.zkn.spring.learn.domain.ProvinceChinaDomain;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +13,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 import javax.sql.RowSet;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by zkn on 2017/9/10.
@@ -27,19 +31,24 @@ public class MockitoDataSource {
         DataSource dataSource = Mockito.mock(DataSource.class);
         Connection con = Mockito.mock(Connection.class);
         Statement stmt = Mockito.mock(Statement.class);
-        RowSet rowSet = Mockito.mock(RowSet.class);
+        ResultSet rowSet = Mockito.mock(ResultSet.class);
         Mockito.when(dataSource.getConnection()).then(t -> {
             System.out.println("getconn ");
             return con;
         });
         Mockito.when(con.createStatement()).thenReturn(stmt);
+        AtomicInteger cursor = new AtomicInteger(1);
+        int size = 3;
+        Mockito.doAnswer((invocation) -> {
+            if (cursor.getAndIncrement() % size == 0) {
+                return false;
+            }
+            return true;
+        }).when(rowSet).next();
         Mockito.when(stmt.executeQuery(Mockito.anyString())).thenReturn(rowSet);
-        //Mockito.verify(rowSet,Mockito.times(1)).next();
-        Mockito.when(rowSet.next()).thenReturn(true);
         Mockito.when(rowSet.getLong(0)).thenReturn(100L);
         Mockito.when(rowSet.getString(1)).thenReturn("zhangsan");
         Mockito.when(rowSet.getString(2)).thenReturn("lisi");
-        Mockito.when(rowSet.next()).thenReturn(false);
         return dataSource;
     }
 
